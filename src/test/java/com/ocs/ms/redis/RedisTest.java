19c9060d23a93c730254c8c40368cb34e0a4c409
joeyee <joeyee@localhost>
2016-12-05 17:33:07 +0800

    修改Application为PassportApplication

---
+++ src/test/java/com/ocs/ms/redis/RedisTest.java
@@ -0,0 +1,17 @@
+package com.ocs.ms.redis;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.ocs.ms.PassportApplication;
+import com.ocs.ms.utils.YamlParser;
+
+@SpringBootTest(classes = PassportApplication.class)
+@WebAppConfiguration
+public class RedisTest {
+
+	@Test
+	public void test1(){
+	}
+}
